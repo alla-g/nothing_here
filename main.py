c9e40ccd1a34a861c4b0c49d8e5bbe33d1f6cb5c
Alla Gorbunova <43267989+alla-g@users.noreply.github.com>
2021-10-16 18:04:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-'''
-логика файлов:
-5 файлов под каждый из пяти способов поиска.
-каждый файл кончается собиранием всего в сёрч/старт функцию
-отд. файл под общие функции: нормализация, препроцессинг,
-собирание данных из файла, поиск близости, сортировка
-
-в main файле весь бэк + вызов нужной функции в зависимости от
-выбранного способа
-
-в файлы способов поиска надо добавить сохранение текстов,
-матриц и векторайзеров а затем их считывание
-'''
-
-
 import json
 import shutup
 shutup.please()
@@ -33,17 +18,21 @@ from bert_functions import index_corpus_bert, search_bert
 
 def prestart(filename):
     '''загрузить модели, посчитать и создать все нужные файлы'''
-    ft_model_path = '../hw4/araneum_none_fasttextcbow_300_5_2018/araneum_none_fasttextcbow_300_5_2018.model'
+    ft_model_path = 'araneum_none_fasttextcbow_300_5_2018.model'
     fasttext_model = KeyedVectors.load(ft_model_path)
+    #fasttext_model = 0
     bert_tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny")
     bert_model = AutoModel.from_pretrained("cointegrated/rubert-tiny")
+    #bert_tokenizer = 0
+    #bert_model = 0
     bert_model = {'bert_tokenizer': bert_tokenizer,
                   'bert_model': bert_model}
-    # collect_answers(filename)
+    collect_answers(filename)
     with open('data/processed_corpus.json', 'r', encoding='UTF-8') as f:
         data = json.load(f)
-    # proc_answers = data['proc_answers']
     raw_answers = data['raw_answers']
+    # одноразовая индексация корпуса:
+    # proc_answers = data['proc_answers']
     # index_corpus_count(proc_answers)
     # index_corpus_tfidf(proc_answers)
     # index_corpus_bm25(proc_answers)
@@ -71,8 +60,12 @@ def start(query, vectorizer, raw_answers, fasttext_model, bert_model):
         order = search_bert(query, **bert_model)
     else:
         raise TypeError('Unknown vectorizer')
-    sorted_answers = arr_answers[order.ravel()]
-    return sorted_answers
+    if order.size:
+        sorted_answers = arr_answers[order.ravel()]
+        return sorted_answers
+    else:
+        return(['О нет, наши мудрецы ещё не задавались таким вопросом. '
+                'Спросите что-нибудь ещё'])
 
 raw_answers, fasttext_model, bert_model = prestart('data/questions_about_love.jsonl')
 #print(start('муж', 'bert', raw_answers, fasttext_model, bert_model))
@@ -82,16 +75,15 @@ st.title('поисковик ответов про любовь')
 st.header('Что вы хотите узнать?')
 st.info('Здесь можно задать самые сокровенные вопросы лучшим умам '
         'человечества, чью мудрость навечно сохранил сайт ответы маил ру. '
-        'Попробуйте') # , вам не понравится')
+        'Попробуйте')
+place_zero = st.empty()
+place_one = st.empty()
+place_two = st.empty()
+place_three = st.empty()
+place_four = st.empty()
 
 
-def search_function(raw_answers, fasttext_model, bert_model):
-    place_zero = st.empty()
-    place_one = st.empty()
-    place_two = st.empty()
-    place_three = st.empty()
-    place_four = st.empty()
-
+def search_function():
     query = ' '
     query = place_zero.text_input('Ваш тревожащий вопрос:', query)
     options = ['count', 'tfidf', 'bm25', 'fasttext', 'bert']
@@ -108,14 +100,14 @@ def search_function(raw_answers, fasttext_model, bert_model):
         start_time = time.time()
         answers = start(query, vectorizer, raw_answers, fasttext_model, bert_model)
         end_time = time.time()
-        time = end_time - start_time
+        seconds = end_time - start_time
         place_one.caption('Ваш ответ:')
         place_two.write('  \n'.join(answers))
-        place_three.caption(f'Поиск сработал за {time} сек')
+        place_three.caption(f'Поиск сработал за {seconds:.4f} сек')
         new_button = place_four.button('Спросить ещё!')
 
         if new_button:
-            search_function(raw_answers, fasttext_model, bert_model)
+            search_function()
 
 
-search_function(raw_answers, fasttext_model, bert_model)
+search_function()
